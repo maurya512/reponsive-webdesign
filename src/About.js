bf2d856e85ff55480f05dbdcff62a9ae4584640e
maurya512 <patelmaurya0512@gmail.com>
2021-05-03 23:41:15 -0400

    tweaked the component layout

--- src/About.js
+++ src/About.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+// import files 
+import web from '../src/images/img2.svg'
+import Common from './Common'
 
 function About() {
     return (
-        <h1>
-            About Page
-        </h1>
+        <Common />
     )
 }
 
--- src/Home.js
+++ src/Home.js
@@ -3,36 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 // import files and images
 import web from '../src/images/img2.svg'
+import Common from './Common';
 function Home() {
     return (
-        <section id='header' className='d-flex align-items-center'>
-            <div className='container-fluid nav_bg'>
-                <div className='row'>
-                    <div className='col-10 mx-auto'>
-                        <div className='row'>
-
-                        <div className='col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column'>
-                            <h1>
-                                Grow your business with <strong className='brand-name' >ESSNTL</strong>
-                            </h1>
-                            <h2 className='my-3'>
-                                We are a team of talented individuals providing solutions to your growing business.
-                            </h2>
-                            <div className='mt-3'>
-                                <NavLink to='/services' className='btn-get-started'>
-                                    Get Started
-                                </NavLink>
-                            </div>
-                        </div>
-
-                        <div className='col-lg-6 order-1 order-lg-2 header-img'>
-                            <img src={web} className='img-fluid animated' alt='home img'/>
-                            </div> 
-                    </div>
-                        </div>
-                </div>
-            </div>
-        </section>
+        <Common />
     )
 }
 
