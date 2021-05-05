e435f1239c3bb72094a42d3c6dd867e3e4a3f123
maurya512 <patelmaurya0512@gmail.com>
2021-05-05 00:21:28 -0400

    a local json file with info to be rendered on the services' cards

---
+++ src/Data.js
@@ -0,0 +1,36 @@
+import web from '../src/images/img1.svg';
+
+// json type data file used to render info to the cars 
+// array of objects
+const Data = [ 
+    {
+        imgsrc: web,
+        title: "Web Development",
+    },
+    {
+        imgsrc: web,
+        title: "Graphic Design",
+    },
+    {
+        imgsrc: web,
+        title: "Affiliate Marketing",
+    },
+    {
+        imgsrc: web,
+        title: "Game Development",
+    },
+    {
+        imgsrc: web,
+        title: "Product Design",
+    },
+    {
+        imgsrc: web,
+        title: "Project Management",
+    },
+    {
+        imgsrc: web,
+        title: "Portrait Photography",
+    }
+]
+
+export default Data
