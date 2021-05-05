740ad0d9f2c9c995f8e05d5e1cf77934873ca2fa
maurya512 <patelmaurya0512@gmail.com>
2021-05-05 00:22:51 -0400

    changed the component to use the data component

--- src/Service.js
+++ src/Service.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
+// import files 
+import Card from './Card'
+import Data from './Data'
+
 function Service() {
     return (
-        <h1>
-            Services Page
-        </h1>
+        <>
+            <div className='my-5'>
+                <h1 className='text-center'>Services that we offer</h1>
+            </div>
+            <div className='container-fluid mb-5'>
+                <div className='row '>
+                    <div className='col-10 mx-auto'>
+                        <div className='row gy-4'>
+                            {/* mapping through the array of objects in Data component */}
+                            {
+                                Data.map((val, ind) => {
+                                    return <Card key={ind}
+                                    imgsrc={val.imgsrc}
+                                    title={val.title}
+                                    />
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
