6c2d889c09302a3f3b570b33c6eed294380a17f2
maurya512 <patelmaurya0512@gmail.com>
2021-05-06 23:41:08 -0400

    changes

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ import Common from './Common'
 function About() {
     return (
         <>
-        {/* pass in props to be accessed in Common component */}
+        {/* pass in props to be accessed in Common component  */}
             <Common name='About us at ' imgsrc={web} visit='/contact' btnname='Contact Now' />
         </>
     )
