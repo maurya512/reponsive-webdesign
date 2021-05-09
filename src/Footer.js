7201db5e300362f98b4135ba053a33fccfbbd835
maurya512 <patelmaurya0512@gmail.com>
2021-05-09 16:59:24 -0400

    Footer component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <footer className='bg-light text-center'>
+                <p>
+                    @2021 ESSNTL. All Rights Reserved. Terms and Conditions Applied.
+                </p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
